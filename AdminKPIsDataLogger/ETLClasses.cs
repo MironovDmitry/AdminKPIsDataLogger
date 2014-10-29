b72b3441742159371804abe209484f14cff7a13c
Dmitry Mironov <dmitry.g.mironov@gmail.com>
2014-10-29 17:31:19 +0300

    initial commin

---
+++ AdminKPIsDataLogger/ETLClasses.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminKPIsDataLogger
+{
+    class Task
+    {
+        public int ID { get; set; }
+        public string Subject { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public Request Request { get; set; }
+        public Admin AssignedAdmin { get; set; }
+        public SLA SLA { get; set; }
+    }
+
+    class Request
+    {
+        public int ID { get; set; }
+        public string Subject { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public Service Service { get; set; }
+    }
+
+    class Service
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Admin
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    class SLA
+    {
+        public int ID { get; set; }
+        public TimeSpan SLATime { get; set; }
+    }
+}
---
+++ AdminKPIsDataLogger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AdminKPIsDataLogger
+{
+    class Program
+    {
+        //define global variables
+        private static string MYSQLCONN = Properties.Resources.MySQLConnectionString;
+        private static string MSSQLCONN = Properties.Resources.MSSQLConnectionString;
+
+        static void Main(string[] args)
+        {
+            MySqlConnection mySQLConn = new MySqlConnection(MYSQLCONN);
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = mySQLConn;
+            cmd.CommandText = @"SELECT  HISTORYID,  WORKORDERID,  OPERATIONOWNERID,  OPERATIONTIME,   OPERATION 
+                                FROM workorderhistory
+                                WHERE WORKORDERID = 79703        
+                                ORDER BY HISTORYID ASC ";
+            mySQLConn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            mySQLConn.Close();
+
+        }
+    }
+}
