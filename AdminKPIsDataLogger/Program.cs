3cbd6d0d8f62006808734274d09a1384f3212176
Dmitry Mironov <dmitry.g.mironov@gmail.com>
2014-10-29 17:50:10 +0300

    Added MySQLDBConnect class to handle MySQL db operations

--- AdminKPIsDataLogger/Program.cs
+++ AdminKPIsDataLogger/Program.cs
@@ -9,23 +9,15 @@ namespace AdminKPIsDataLogger
 {
     class Program
     {
-        //define global variables
-        private static string MYSQLCONN = Properties.Resources.MySQLConnectionString;
-        private static string MSSQLCONN = Properties.Resources.MSSQLConnectionString;
-
         static void Main(string[] args)
         {
-            MySqlConnection mySQLConn = new MySqlConnection(MYSQLCONN);
-            MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = mySQLConn;
-            cmd.CommandText = @"SELECT  HISTORYID,  WORKORDERID,  OPERATIONOWNERID,  OPERATIONTIME,   OPERATION 
-                                FROM workorderhistory
-                                WHERE WORKORDERID = 79703        
-                                ORDER BY HISTORYID ASC ";
-            mySQLConn.Open();
-            MySqlDataReader reader = cmd.ExecuteReader();
-            mySQLConn.Close();
+            Start();
+            
+        }
 
+        public static void Start()
+        { 
+            
         }
     }
 }
